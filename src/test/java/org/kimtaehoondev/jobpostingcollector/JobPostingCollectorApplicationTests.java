6c8b2cba08581994ccc2316f757bcdb7aaa36427
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-02 14:35:10 +0900

    init(): 초기화

---
+++ src/test/java/org/kimtaehoondev/jobpostingcollector/JobPostingCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kimtaehoondev.jobpostingcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPostingCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
