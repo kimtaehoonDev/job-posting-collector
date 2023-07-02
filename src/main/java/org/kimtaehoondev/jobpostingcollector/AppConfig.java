5ebd293c4c37dade2729bc5dc32a2ea24e74c5bf
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-02 15:25:54 +0900

    feat(AppConfig): WebDriver 빈으로 등록

---
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/AppConfig.java
@@ -0,0 +1,22 @@
+package org.kimtaehoondev.jobpostingcollector;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    WebDriver webDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        options.setHeadless(true);
+        options.addArguments("--lang=ko");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        return new ChromeDriver(options);
+    }
+}
