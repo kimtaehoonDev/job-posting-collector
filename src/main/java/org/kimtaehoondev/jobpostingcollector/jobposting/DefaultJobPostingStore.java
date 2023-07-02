cb6d1a75535b424367f6b495e2f51423c8f514c4
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-02 15:46:45 +0900

    feat(DefaultJobPostingStore): 객체 비어있는 상태로 생성

---
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/jobposting/DefaultJobPostingStore.java
@@ -0,0 +1,12 @@
+package org.kimtaehoondev.jobpostingcollector.jobposting;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultJobPostingStore implements JobPostingStore {
+    @Override
+    public void updateAll(List<JobPosting> result) {
+
+    }
+}
