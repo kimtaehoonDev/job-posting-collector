b2262c1241b8f97305557ab9c0a019b67ee19c91
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-03 12:58:59 +0900

    feat(JobPosting): createdAt 필드 추가

--- src/main/java/org/kimtaehoondev/jobpostingcollector/jobposting/JobPosting.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/jobposting/JobPosting.java
@@ -1,15 +1,25 @@
 package org.kimtaehoondev.jobpostingcollector.jobposting;
 
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.ToString;
 
-@Builder
 @ToString
 public class JobPosting {
     private final String occupation;
     private final String companyName;
     private final URL link;
     private final List<String> infos;
+    private final LocalDateTime createdAt;
+
+    @Builder
+    private JobPosting(String occupation, String companyName, URL link, List<String> infos) {
+        this.occupation = occupation;
+        this.companyName = companyName;
+        this.link = link;
+        this.infos = infos;
+        this.createdAt = LocalDateTime.now();
+    }
 }
