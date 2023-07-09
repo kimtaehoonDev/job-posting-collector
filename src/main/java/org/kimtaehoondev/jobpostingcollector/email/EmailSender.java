cb6408a0c3191994d9b2faa187aceb0598ae6360
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-09 18:18:22 +0900

    feat(EmailSender): EmailSender의 sendAll 구현

--- src/main/java/org/kimtaehoondev/jobpostingcollector/email/EmailSender.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/email/EmailSender.java
@@ -1,7 +1,59 @@
 package org.kimtaehoondev.jobpostingcollector.email;
 
-public class EmailSender {
-    public void sendToAll(String message) {
 
+import java.time.LocalDate;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.kimtaehoondev.jobpostingcollector.email.repository.EmailRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailSender {
+    private final EmailConfig emailConfig;
+    private final Properties emailProperties;
+    private final EmailRepository emailRepository;
+
+    public void sendToAll(String content) {
+        for (String email : emailRepository.findAll()) {
+            send(email, content);
+        }
+    }
+
+    private void send(String email, String content) {
+        try {
+            Session session = getSession();
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(emailConfig.getUsername()));
+            message.addRecipient(Message.RecipientType.TO,
+                new InternetAddress(email));
+            message.setSubject(LocalDate.now()+ "일자 채용 안내 - 김태훈");
+            message.setText(content, emailConfig.getEncType()); // HTML 내용
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Session getSession() {
+        return Session.getDefaultInstance(emailProperties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailConfig.getUsername(), emailConfig.getPassword());
+            }
+        });
     }
 }
---
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/email/repository/EmailRepository.java
@@ -0,0 +1,7 @@
+package org.kimtaehoondev.jobpostingcollector.email.repository;
+
+import java.util.List;
+
+public interface EmailRepository {
+    List<String> findAll();
+}
