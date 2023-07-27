4c97b56c865af736a811608b85817480efabd30c
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-27 16:02:25 +0900

    feat(index.html): GET /email 링크 연결

--- src/main/java/org/kimtaehoondev/jobpostingcollector/jobposting/community/impl/WantedCommunity.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/jobposting/community/impl/WantedCommunity.java
@@ -3,7 +3,6 @@ package org.kimtaehoondev.jobpostingcollector.jobposting.community.impl;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.kimtaehoondev.jobpostingcollector.jobposting.community.JobPostingCommunity;
 import org.kimtaehoondev.jobpostingcollector.jobposting.community.JobPostingCommunityType;
 import org.kimtaehoondev.jobpostingcollector.jobposting.dto.request.JobPostingData;
@@ -16,7 +15,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class WantedCommunity implements JobPostingCommunity {
     private JobPostingCommunityType communityType = JobPostingCommunityType.WANTED;
     private Status status = Status.GOOD;
