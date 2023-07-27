8cf5cc9e1cfd85d69aa47559a56b677b8d42dbeb
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-27 15:33:35 +0900

    feat(WebConfig): 크롬 옵션 추가

--- src/main/java/org/kimtaehoondev/jobpostingcollector/config/WebConfig.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/config/WebConfig.java
@@ -14,10 +14,14 @@ public class WebConfig {
         ChromeOptions options = new ChromeOptions();
         options.setCapability("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         options.setHeadless(true);
+        options.addArguments("--no-sandbox");
         options.addArguments("--lang=ko");
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("user-agent=Mozilla/5.0");
+        options.addArguments("--incognito"); // 시크릿모드
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-extensions");
 
         return new ChromeDriver(options);
     }
