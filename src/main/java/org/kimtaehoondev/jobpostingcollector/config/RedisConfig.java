1140905dd96179ebde0cdbd21eaad230ef646e9c
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-28 22:04:19 +0900

    feat(RedisConfig): RedisConfig 설정

--- src/main/java/org/kimtaehoondev/jobpostingcollector/config/RedisConfig.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package org.kimtaehoondev.jobpostingcollector.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -11,11 +15,36 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 @RequiredArgsConstructor
 public class RedisConfig {
+    private final RedisProperties redisProperties;
+    private final RedisCustomProperties redisCustomProperties;
+
     @Bean
-    public RedisTemplate<?, ?> redisTemplate (RedisConnectionFactory connectionFactory) {
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+            new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactorySub() {
+        return new LettuceConnectionFactory(
+            new RedisStandaloneConfiguration(
+                redisCustomProperties.getHost(),
+                redisCustomProperties.getPort()));
+    }
+
+    @Bean // 디폴트
+    public RedisTemplate<?, ?> redisTemplate(@Qualifier("redisConnectionFactory")
+                                                  RedisConnectionFactory connectionFactory) {
         RedisTemplate<?, ?> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 
+    @Bean
+    public RedisTemplate<?, ?> redisTemplateSub(@Qualifier("redisConnectionFactorySub")
+                                                   RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
         return template;
     }
 }
