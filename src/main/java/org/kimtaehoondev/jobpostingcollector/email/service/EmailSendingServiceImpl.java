aeb683ad0751218c44daf9da60fbfb9d69deba46
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-28 18:36:50 +0900

    refactor(EmailSendingServiceImpl): 이메일로 채용공고를 보낼 때 타임리프를 사용하도록 변경

--- src/main/java/org/kimtaehoondev/jobpostingcollector/email/service/EmailSendingServiceImpl.java
+++ src/main/java/org/kimtaehoondev/jobpostingcollector/email/service/EmailSendingServiceImpl.java
@@ -1,7 +1,10 @@
 package org.kimtaehoondev.jobpostingcollector.email.service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.StringJoiner;
 import lombok.RequiredArgsConstructor;
 import org.kimtaehoondev.jobpostingcollector.email.EmailSender;
@@ -9,17 +12,26 @@ import org.kimtaehoondev.jobpostingcollector.email.dto.response.EmailResponseDto
 import org.kimtaehoondev.jobpostingcollector.email.repository.EmailRepository;
 import org.kimtaehoondev.jobpostingcollector.jobposting.dto.response.JobPostingResponseDto;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
 
 @Service
 @RequiredArgsConstructor
 public class EmailSendingServiceImpl implements EmailSendingService {
     private final EmailRepository emailRepository;
+    private final TemplateEngine templateEngine;
     private final EmailSender emailSender;
 
     @Override
     public void sendJobPostings(List<JobPostingResponseDto> jobPostings) {
         String title = LocalDate.now() + "일자 채용 안내 - 김태훈";
-        String message = makePrettierHtml(jobPostings);
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("postings", jobPostings);
+        Context context = new Context(Locale.KOREAN, variables);
+        context.setVariable("title", title);
+        String message =
+            templateEngine.process("/email/job-posting.html", context);
         List<EmailResponseDto> total = emailRepository.findAllBy();
         for (EmailResponseDto emailResponseDto : total) {
             emailSender.sendHtml(emailResponseDto.getEmail(), title, message);
@@ -33,28 +45,6 @@ public class EmailSendingServiceImpl implements EmailSendingService {
     }
 
 
-    private String makePrettierHtml(List<JobPostingResponseDto> postings) {
-
-        StringJoiner sj = new StringJoiner("\n");
-        sj.add("<h1>" + LocalDate.now() + "</h1>");
-        sj.add("<h2>새롭게 등록된 직무 목록</h2>");
-        for (JobPostingResponseDto posting : postings) {
-            sj.add("<div>" + makePrettierHtml(posting)+ "</div>");
-        }
-        return sj.toString();
-    }
-
-    private String makePrettierHtml(JobPostingResponseDto posting) {
-        StringJoiner sj = new StringJoiner("\n");
-        sj.add("<div>");
-        sj.add("<p>직종: " + posting.getOccupation()+"</p>");
-        sj.add("<p>회사명: " + posting.getCompanyName()+"</p>");
-        sj.add("<p>" + posting.getLink().toString()+"</p>");
-        sj.add("<p>" + posting.getInfos() + "</p>");
-        sj.add("</div>");
-        sj.add("<hr>");
-        return sj.toString();
-    }
 
     private void validateEmailDuplicated(String email) {
         boolean isDuplicated = emailRepository.existsByEmail(email);
