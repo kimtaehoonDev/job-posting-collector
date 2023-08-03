ebd32015d7f7c0804ae2e561c92614b1ef861043
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-03 21:42:05 +0900

    fix(): css 적용되지 않는 이슈 해결

---
+++ src/main/java/org/kimtaehoondev/jpcollector/config/StaticConfig.java
@@ -0,0 +1,15 @@
+package org.kimtaehoondev.jpcollector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class StaticConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/");
+    }
+}
