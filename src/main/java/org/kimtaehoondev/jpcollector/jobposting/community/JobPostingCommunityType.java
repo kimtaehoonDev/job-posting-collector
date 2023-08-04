7129591c7ab7894193a03f3464881bdaf704f020
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-04 13:48:00 +0900

    refactor(JobPostingCommunityType): 로켓펀치 필터링 조건 변경

--- src/main/java/org/kimtaehoondev/jpcollector/jobposting/community/JobPostingCommunityType.java
+++ src/main/java/org/kimtaehoondev/jpcollector/jobposting/community/JobPostingCommunityType.java
@@ -7,7 +7,7 @@ import org.kimtaehoondev.jpcollector.utils.UrlParser;
 @Getter
 public enum JobPostingCommunityType {
     JOB_PLANET("https://www.jobplanet.co.kr/job"),
-    ROCKET_PUNCH("https://www.rocketpunch.com/jobs?job=1&specialty=Java&career_type=1&career_type=4&tag=%EC%9B%B9%EC%84%9C%EB%B9%84%EC%8A%A4"),
+    ROCKET_PUNCH("https://www.rocketpunch.com/jobs?job=1&tag=%EC%9B%B9%EC%84%9C%EB%B9%84%EC%8A%A4&career_type=1&career_type=4&keywords=java&keywords=spring"),
     WANTED("https://www.wanted.co.kr/wdlist/518/872?country=kr&job_sort=company.response_rate_order&years=0");
 
     private URL url;
